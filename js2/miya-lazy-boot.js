918e6de85d8d798b7fbe13ea60a3d51f0b2b7843
Karin-ISTJ <heyingkws@163.com>
2026-09-25 15:12:31 +0800

    .

--- js2/miya-lazy-boot.js
+++ js2/miya-lazy-boot.js
@@ -46,7 +46,7 @@
       ]
     },
     memoryUi: {
-      css: ['css/miya-memory.css?v=11'],
+      css: ['css/miya-memory.css?v=12'],
       js: ['js2/miya-memory-app.js?v=13']
     },
     funUi: {
